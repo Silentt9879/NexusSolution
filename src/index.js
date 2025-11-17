f44ef8fd55078536ac88dc647eabfac1c3a1d348
Silentt9879 <jayvianlazarus120904@gmail.com>
2025-11-17 11:00:40 +0800

    Implement professional i18next translation

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import { HelmetProvider } from 'react-helmet-async'; // 👈 1. IMPORT
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 import App from './App';
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <HelmetProvider> {/* 👈 2. WRAP YOUR APP */}
-        <BrowserRouter basename="/NexusSolution">
-          <App />
-        </BrowserRouter>
-      </HelmetProvider> {/* 👈 2. END WRAP */}
+      <React.Suspense fallback="Loading...">
+        <HelmetProvider> {/* 👈 2. WRAP YOUR APP */}
+          <BrowserRouter basename="/NexusSolution">
+            <App />
+          </BrowserRouter>
+        </HelmetProvider> {/* 👈 2. END WRAP */}
+      </React.Suspense>
     </ThemeProvider>
   </React.StrictMode>
 );
